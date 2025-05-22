113aa31c353c61e88d98e271120e04724a1155fb
Andrew Barnett <138056529+AndrewMBarnett@users.noreply.github.com>
2025-05-22 18:54:20 -0500

    Updated EAs

--- Extension Attributes/DemoteLDEnabled.sh
+++ Extension Attributes/DemoteLDEnabled.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-LABEL="com.demote.demoteadmins"
-if launchctl list | grep -qw "$LABEL"; then
+# This EA checks if the LaunchDaemon for demoting unauthorized admins is loaded.
+LAUNCHD_LABEL="com.demote.demoteadmins"
+PLIST="/Library/LaunchDaemons/com.demote.demoteadmins.plist"
+
+if [ ! -f "$PLIST" ]; then
+    echo "<result>LaunchDaemon plist Not Found</result>"
+    exit 0
+fi
+
+if launchctl list | grep -qw "$LAUNCHD_LABEL"; then
     echo "<result>Loaded</result>"
 else
     echo "<result>Not Loaded</result>"
--- Extension Attributes/DemoteLog.sh
+++ Extension Attributes/DemoteLog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script checks the last entry in the demote log file and returns it as a result. It is used to provide information about the last demote operation.
+
+# Check if the log file exists and read the last entry
 LOG="/Library/Management/demoter/logs/demoteadmins.log"
 if [ -f "$LOG" ]; then
     last=$(tail -1 "$LOG" 2>/dev/null)
